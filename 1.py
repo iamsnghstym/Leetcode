3d5bdc4258c2c6b40e9ef2220304bc2b042821b5
Satyam Singh <satyamsingh@Satyam-ka-MacBook-Air.local>
2025-02-12 14:06:56 +0530

    1. Two Sum

---
+++ 1.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = dict()
+        for idx, value in enumerate(nums):
+            to_find = target - value
+            if to_find in dictionary:
+                return [idx, dictionary[to_find]]
+            dictionary[value] = idx
+        return []
